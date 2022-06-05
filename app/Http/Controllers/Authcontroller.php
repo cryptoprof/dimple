f63450c9c83dc6f5797364952c164ef992ffe90a
Cryptoprof <cryptoprof@gmail.com>
2022-06-05 15:34:29 +0300

    sanctum api auth

---
+++ app/Http/Controllers/Authcontroller.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class Authcontroller extends Controller
+{
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            "email" => 'required|string',
+            "password" => 'required|string',
+        ]);
+
+        // Check Email
+        $where = ["email"=>$fields['email']];
+        $user = User::where($where)->first();
+
+        // Check Password
+        if(!$user || !Hash::check($fields['password'], $user->password))
+        {
+            return response([
+                'message' => "Bad credentials"
+            ],401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user'=>$user,
+            'token'=>$token
+        ];
+
+        return response($response,201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        $response = [
+            'message' => 'logged out'
+        ];
+
+        return response($response,201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -13,11 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Public routes
+Route::post('/login', [App\Http\Controllers\AuthController::class,'login']);
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [App\Http\Controllers\AuthController::class,'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
 Route::group(['prefix' => '/v1', 'namespace' => 'App\Http\Controllers\Api\V1', 'as' => 'api.'], function () {
-    Route::group(['middleware' => ['role:admin']], function () {
+    Route::group(['middleware' => ['auth:sanctum','role:admin']], function () {
         Route::resource('users', 'UsersController');
     });
 });
